3c4ad1509cd0a8c1da3aaf8b6848f496b766019b
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-05-04 11:40:29 +0530

    done

--- Web View/ViewController.swift
+++ Web View/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        webView.loadRequest(URLRequest(url: URL(string: "https://www.youtube.com/")!))
     }
 
 
